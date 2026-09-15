97110e5802beee3709ea4c15c22732fa276f466c
yuyuaww <yuyua1024@gmail.com>
2026-09-15 09:59:01 +0800

    20.111

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false      // 新增
+    alias(libs.plugins.kotlin.android) apply false       // 新增
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
 }
